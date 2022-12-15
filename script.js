78fd25598d87f19886d6f72f66b49a9e7c28c974
Araceli Gittardi <araceligittardi@tdf.edu.ar>
2022-12-15 11:26:06 -0300

    terminado

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const api = 'https://6393e57e11ed187986bf9667.mockapi.io/api/curso/employees';
 function newList(data){   
     for (let i = 0; i < data.length; i++) {
         let element = data[i];
-        // creo los elementos dentro del tbody
+        
         let tr = document.createElement('tr');
         let tdId = document.createElement('td');
         let tdName = document.createElement('td');
@@ -12,18 +12,19 @@ function newList(data){
         let tdMail = document.createElement('td');
         let tdEdit = document.createElement('td');
         let tdDelete = document.createElement('td');
-        // le inserto los valores al tr y td
+        
         tdId.innerText = element.id;
         tdName.innerText = element.name;
         tdCity.innerText = element.city;
         tdBday.innerText = element.birthday;
         tdMail.innerText = element.email;
-        tdEdit.innerHTML = `<button type="button" id="${element.id}" onclick="edit('${element.id}','${element.name}','${element.city}', '${element.birthday}','${element.email}')" class="btn btn-dark">Edit</button>`;
-        tdDelete.innerHTML = `<button type="button" id="${element.id}" onclick="deleteEmployee('${element.id}')" class="btn btn-light">Delete</button>`;
-        // agarro el tbody y voy insertando en el html 
+      
+        tdEdit.innerHTML = `<button type = "button" id = "${element.id}" onclick = "edit('${element.id}','${element.name}','${element.city}','${element.birthday}','${element.email}')" class = "btn btn-dark">Edit</button>`;
+        tdDelete.innerHTML = `<button type = "button" id = "${element.id}" onclick = "deleteEmployee('${element.id}')" class = "btn btn-light">Delete</button>`;
+         
         let tbody = document.getElementById('tab');
 
-        tr.id = 'row'+element.id;
+        tr.id = 'row'+ element.id;
 
         tbody.appendChild(tr);
         tr.appendChild(tdId);
@@ -40,8 +41,7 @@ function edit(id, name, city, birthday, email) {
 
     let idForm = document.getElementById('idForm');
     idForm.value = id;
-    
-    // aqui leo las id del formulario
+
     let nameForm = document.getElementById('nameForm');
     let cityForm = document.getElementById('cityForm');
     let birthdayForm = document.getElementById('birthdayForm');
@@ -50,45 +50,22 @@ function edit(id, name, city, birthday, email) {
     cityForm.value = city;
     birthdayForm.value = birthday;
     emailForm.value = email;
-        
+//    togglear();    
 }
-
+/*function togglear(){
+    document.querySelector(".idForm").addEventListener('click',()=>{
+    classList.toggle("hide");
+});
+}*/
 function deleteEmployee(id){
    
-    let out = document.getElementById('row'+id).remove();
+    let out = document.getElementById('row'+ id).remove();
     alert(`Delete successfully: ${id }`);
 
 }
 
-function save(){
+function add(){
 
-     let idF = document.getElementById('idForm');   
-     let nameF = document.getElementById('nameForm');
-     let cityF = document.getElementById('cityForm');
-     let birthdayF = document.getElementById('birthdayForm');
-     let emailF = document.getElementById('emailForm');
-
-     // le inserto los valores al tr y td
-     let tr = document.getElementById('row'+ idF.value);
-
-     let id =  tr.firstElementChild;
-     id.nextElementSibling.innerText = nameF.value;
-
-     let name= id.nextElementSibling;
-     name.nextElementSibling.innerText = cityF.value;
-
-     let city = name.nextElementSibling;
-     city.nextElementSibling.innerText = birthdayF.value;
-     
-     let birthday = city.nextElementSibling;
-     birthday.nextElementSibling.innerText = emailF.value;
-
-
-}
-
-function agregar(){
-
-    // creo los elementos dentro del tbody
     let tr = document.createElement('tr');
     let tdId = document.createElement('td');
     let tdName = document.createElement('td');
@@ -98,7 +75,6 @@ function agregar(){
     let tdEdit = document.createElement('td');
     let tdDelete = document.createElement('td');
 
-    // le inserto los valores al tr y td
     let tabla = document.getElementById('tab');
     let newId = parseInt(tabla.lastChild.firstChild.innerText)+ 1;
 
@@ -116,13 +92,12 @@ function agregar(){
     let emailF = document.getElementById('emailForm');
     tdMail.innerText = emailF.value;
 
-
-    tdEdit.innerHTML = `<button type="button" id="${newId}" onclick="edit('${newId}','${nameF.value}','${cityF.value}','${birthdayF.value}','${emailF.value}')" class="btn btn-dark">Edit</button>`;
-    tdDelete.innerHTML = `<button type="button" id="${newId}" onclick="deleteEmployee('${newId}')" class="btn btn-light">Delete</button>`;
-    // agarro el tbody y voy insertando en el html 
+    tdEdit.innerHTML = `<button type = "button" id = "${newId}" onclick = "edit('${newId}','${nameF.value}','${cityF.value}','${birthdayF.value}','${emailF.value}')" class = "btn btn-dark">Edit</button>`;
+    tdDelete.innerHTML = `<button type = "button" id="${newId}" onclick = "deleteEmployee('${newId}')" class = "btn btn-light">Delete</button>`;
+   
     let tbody = document.getElementById('tab');
 
-    tr.id = 'row'+newId;
+    tr.id = 'row'+ newId;
 
     tbody.appendChild(tr);
     tr.appendChild(tdId);
@@ -134,6 +109,30 @@ function agregar(){
     tr.appendChild(tdDelete);
 }
 
+function save(){
+
+    let idF = document.getElementById('idForm');   
+    let nameF = document.getElementById('nameForm');
+    let cityF = document.getElementById('cityForm');
+    let birthdayF = document.getElementById('birthdayForm');
+    let emailF = document.getElementById('emailForm');
+
+    let tr = document.getElementById('row'+ idF.value);
+
+    let id =  tr.firstElementChild;
+    id.nextElementSibling.innerText = nameF.value;
+
+    let name = id.nextElementSibling;
+    name.nextElementSibling.innerText = cityF.value;
+
+    let city = name.nextElementSibling;
+    city.nextElementSibling.innerText = birthdayF.value;
+    
+    let birthday = city.nextElementSibling;
+    birthday.nextElementSibling.innerText = emailF.value;
+
+}
+
 fetch(api)
     .then(response => response.json())
     .then(data => {
@@ -142,104 +141,54 @@ fetch(api)
   
     .catch(error => console.error(error))
 
-/*const api = 'https://6393e57e11ed187986bf9667.mockapi.io/api/curso/employees';
-
-let arrayData;
-
-
-fetch(api)
-    .then(response => response.json())
-    .then(data => {
-        arrayData = data
-    /*console.log(`Name: ${data.results[0].name} City: ${data.results[0].city} Birthday: ${data.results[0].birthday} Email: ${data.results[0].email} ID: ${data.results[0].id}`)
-    for (let i = 0; i < data.results.length; i++) {
-      const element = data.results[i];
-      console.log(element);
-   for (let i = 0; i < arrayData.length; i++) {
-        //let arrayData = data[i];
-        //console.log(arrayData)
-      let tbody = document.getElementById('tab');   
-      let tr = document.createElement('tr');
-      let thID = document.createElement('th');
-      let tdName = document.createElement('td');
-      let tdCity = document.createElement('td');
-      let tdBirthday = document.createElement('td');
-      let tdEmail = document.createElement('td');
-            
-      tdName.innerText = data[i].name;
-      tdCity.innerText = data[i].city;
-      tdBirthday.innerText = data[i].birthday;
-      tdEmail.innerText = data[i].email;
-      thID.innerText = data[i].id;
-    
-      tbody.appendChild(tr);
-      tr.appendChild(tdName);
-      tr.appendChild(tdCity);
-      tr.appendChild(tdBirthday);
-      tr.appendChild(tdEmail);
-      tr.appendChild(thID);
-      
-      let btnSearch = document.getElementById('search');
-      let btnEdit = document.getElementById('edit');
-      let btnDelete = document.getElementById('delete');
-      
-      btnEdit.innerText = "Edit";
-      btnDelete.innerText = "Delete";
-  
-      btnEdit.addEventListener("click", () => {
-        let edit = btnEdit.parentElement;
-        edit = edit.childNodes
-        if (btnEdit.innerText == "Editar") {
-            editar(edit);
-            btnEdit.innerText = "Aceptar";
-        } else {
-            guardarCambio(edit)
-            btnEdit.innerText = "Editar";
-        }
-    });
-    //añadiendo funcionalidad eliminar
-      btnDelete.addEventListener("click", () => {
-          tr.remove();
-      });
-
-    function editar(filas) {
-        filas.forEach(elemento => {
-            if (elemento.className === "edit") {
-                let celdasEditables = document.createElement("input");
-                celdasEditables.value = elemento.innerText;
-                elemento.replaceWith(celdasEditables);
-                celdasEditables.classList.add("edit")
-            }
-        })
-    }
-
-function guardarCambio(filas) {
-    filas.forEach(elemento => {
-        if (elemento.className === "edit") {
-            let celdaFinal = document.createElement("td");
-            celdaFinal.innerText = elemento.value;
-            elemento.replaceWith(celdaFinal);
-            celdaFinal.classList.add("edit")
-        };
-    });
-};
-}
+/*  POTENCIALES MEJORAS - SI TUVIERA MÁS TIEMPO -:
+1. DESPLEGAR UN FORMULARIO APARTE PARA EDITAR,COMO PEDÍA LA CONSIGNA.
+2. O BIEN, TOGGLEAR EL ÚLTIMO DIV DEL DOM PARA QUE APAREZCA ÚNICAMENTE CUANDO SE BUSCA EDITAR UN CAMPO.
+3. AGREGAR MÁS ESTILOS.
+4. AGREGAR Y PROBAR LAS SIGUIENTES FUNCIONALIDADES EN SAVE():
+let btnSave = document.querySelector('.btnSave');
+btnSave.addEventListener('click', ()=> {
+validateName();
+validateCity();
+validateBirthday();
+validateEmail();
 })
-  .catch(err => console.log('Hubo un problema con la petición Fetch:' + err.message))
 
-let inputID = document.getElementById('id');
-let btnSearch = document.getElementById('search');
-let inputName = document.getElementById('name');
-let inputCity = document.getElementById('city');
-let inputBtd = document.getElementById('birthday');
-let inputEmail = document.getElementById('email');
-let btnEdit = document.getElementById('edit');
-let btnDelete = document.getElementById('delete');
-
-
-  
-
-
-
-
+function validateName() {
+let nameF = document.querySelector('.name').value;
+    if ((nameF! = '') && (nameF.length >= 3) && (nameF.length <= 20) && (nameF != 'undefined')){
+        let id =  tr.firstElementChild;
+        id.nextElementSibling.innerText = nameF.value;
+    } else {
+        alert('Not valid: Name');
+    }
+}
+function validateCity() {
+let cityF = document.querySelector('.city').value;
+    if ((cityF != '') && (cityF.length >= 3) && (cityF.length <= 20) && (cityF != 'undefined')) {
+        let name = id.nextElementSibling;
+        name.nextElementSibling.innerText = cityF.value;    
+    } else {
+        alert('Not valid: city');
+    }
+}
+function validateBirthday() {
+let birthdayF = document.querySelector('.mail').value;
+    if (birthdayF != '') { 
+        let city = name.nextElementSibling;
+    city.nextElementSibling.innerText = birthdayF.value;
+    } else {
+        alert('Not valid: Birthday');
+    }
+}
+function validateEmail() {
+let emailF = document.querySelector('.mail').value;
+    if (emailF != '') { 
+        let birthday = city.nextElementSibling;
+        birthday.nextElementSibling.innerText = emailF.value;
+    } else {
+        alert('Not valid: Email');
+    }
+}
+}
 */
